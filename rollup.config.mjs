df9e97e3dbd5ba0736b74bbff0fcec6c458d6632
Javier Prieto <javier.accounts@fiml.co>
2024-03-28 13:35:08 -0400

    fix: renamed rollup config file and added more sustainable package.json reading approach

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,29 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import dts from '@rollup-plugin-dts'
+import dts from 'rollup-plugin-dts'
+import { readFileSync } from 'node:fs';
 
-const packageJson = require('./package.json');
+const packageJson = JSON.parse(
+	readFileSync(new URL('package.json', import.meta.url))
+);
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson,main,
+        file: packageJson.main,
         format: 'cjs',
-        sourcemap: true``
+        sourcemap: true
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true``
+        sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig/json' })]
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
   },
   {
     input: 'dist/esm/types/index.d.ts',
