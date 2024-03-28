c2b1bdc7b22fcc1ee7560b0c54ae792a5a8e7d1e
Javier Prieto <javier.accounts@fiml.co>
2024-03-28 15:19:43 -0400

    chore: formats rollup config file.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,20 +16,25 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true
-      }
+        sourcemap: true,
+      },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'] }),
+      postcss({ extensions: ['.css'], inject: true, extract: false }),
+    ],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.css$/]
-  }
+    external: [/\.css$/],
+  },
 ];
