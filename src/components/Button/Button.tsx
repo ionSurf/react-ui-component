4ec6cbe43e53b74f625a1ca733899abd7915f737
Javier Prieto <javier.accounts@fiml.co>
2024-03-28 15:18:24 -0400

    fix: upgrades button to use CSS

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,29 @@
+import './Button.css';
+
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary'
 }
 
 const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
-  const colorClass = variant === 'primary' ? 'blue' : 'red';
+  const colorClass = () => {
+    if (variant === 'primary') {
+      return {
+          borderColor: '#6366F1',
+          backgroundColor: '#6366F1',
+          color: '#FAFAFA'
+        }
+    }
+    if (variant === 'secondary') {
+      return {
+          borderColor: '#6366F1',
+          backgroundColor: '#FAFAFA',
+          color: '#6366F1'
+        }
+    }
+    return {}
+  }
   return (
-  <button style={{backgroundColor: colorClass}} {...props}>
+  <button style={{ ...colorClass(), ...props.style }} {...props}>
     {props.children}
   </button>
 )}
